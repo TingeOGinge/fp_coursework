2116b85368542c9a388fc6e8d2db878c650c67bc
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-04-16 19:14:14 +0100

    implement functioning UI

--- UP887062.hs
+++ UP887062.hs
@@ -4,6 +4,7 @@
 --
 
 import Text.Printf
+import Text.Read
 import Data.List
 
 --
@@ -94,6 +95,18 @@ convertYCoord n = round (50 - ((n - 48.2) * (50 / 12)))
 convertXCoord :: Float -> Int
 convertXCoord n = round ((n + 7.4) * (80 / 8))
 
+isPlace :: String -> Place -> Bool
+isPlace s (Place n c r) = s == n
+
+placeExists :: String -> [Place] -> Bool
+placeExists s = foldr (||) False . map(isPlace s)
+
+validInt :: String -> Bool
+validInt s = Nothing /= (readMaybe s :: Maybe Int)
+
+validFloat :: String -> Bool
+validFloat s = Nothing /= (readMaybe s :: Maybe Float)
+
 --
 --  Demo
 --
@@ -106,7 +119,7 @@ demo 1 = putStr (displayCityNames testData)
 demo 2 = putStrLn (placeAvgToString "Cardiff" testData)
 
 -- display all place names and their 7-day rainfall figures as a single string
-demo 3 = putStrLn (placesToString testData)
+demo 3 = putStr (placesToString testData)
 
 -- display the names of all places that were dry two days ago
 demo 4 = putStr(displayCityNames (dryPlacesInXDays 2 testData))
@@ -130,17 +143,15 @@ demo 7 = do
   let closestPlace = getClosest (50.9, (-1.2)) (dryPlacesInXDays 1 testData)
   putStrLn (placesToString [closestPlace])
 
-
+-- Display a rainfall map showing all places relative to their locations
 demo 8 = do
   clearScreen
   displayMap testData
 
 
 --
--- Screen Utilities (use these to do the rainfall map - note that these do
--- not work in WinGHCi on Windows, so use GHCi.)
---
-
+-- Screen Utilities (use these to do the rainfall map - note that
+-- these do not work in WinGHCi on Windows, so use GHCi.)
 --
 
 type ScreenPosition = (Int,Int)
@@ -167,12 +178,155 @@ writeAt position text = do
 displayMap :: [Place] -> IO()
 displayMap [] = goTo(0, 50)
 displayMap ((Place n (y, x) r):xs) = do
-  let output = placeAvgToString n [(Place n (x, y) r)]
+  let output = placeAvgToString n [(Place n (y, x) r)]
   writeAt (convertXCoord x, convertYCoord y) output
   displayMap xs
-
-
 
 --
 -- Your user interface (and loading/saving) code goes here
 --
+
+getRainfallUpdates :: [Place] -> [Int] -> IO [Int]
+getRainfallUpdates [] rainfallFigs = return rainfallFigs
+getRainfallUpdates ((Place name c r):xs) rainfallFigs = do
+  putStr (printf "Enter the rainfall figure for %s: " name)
+  i <- getLine
+  if (validInt i) then do
+    let newList = rainfallFigs ++ [(read i :: Int)]
+    getRainfallUpdates xs newList
+    else do
+      putStrLn "Invalid input"
+      getRainfallUpdates ((Place name c r):xs) rainfallFigs
+
+getCoords :: IO Coords
+getCoords = do
+  putStr "Enter (N): "
+  n <- getLine
+  putStr "Enter (E): "
+  e <- getLine
+  if (validFloat n && validFloat e) then
+    return ((read n :: Float), (read e :: Float))
+    else do
+      putStrLn "Invalid input"
+      getCoords
+
+getRainfall :: Int -> [Int] -> IO [Int]
+getRainfall 0 rainfall = return rainfall
+getRainfall n rainfall = do
+  putStr "Input rainfall data: "
+  r <- getLine
+  if (validInt r) then do
+    let newRainfall = rainfall ++ [(read r :: Int)]
+    getRainfall (n-1) newRainfall
+    else do
+      putStrLn "Invalid input"
+      getRainfall n rainfall
+
+createPlace :: IO Place
+createPlace = do
+  putStr "Enter the name of the new place: "
+  name <- getLine
+  coords <- getCoords
+  rainfall <- getRainfall 7 []
+  return (Place name coords rainfall)
+
+option :: String -> [Place] -> IO [Place]
+option "1" list = do
+  putStr (displayCityNames list)
+  return list
+
+option "2" list = do
+  putStr "Enter the name of your place: "
+  place <- getLine
+  if (placeExists place list) then do
+    putStrLn (placeAvgToString place list)
+    return list
+    else do
+      putStrLn (printf "%s was not found, please try again" place)
+      option "2" list
+
+option "3" list = do
+  putStr (placesToString list)
+  return list
+
+option "4" list = do
+  putStrLn "How many days ago?"
+  days <- getLine
+  if (not (validInt days) || days < "1" || days > "7") then do
+    putStrLn "Invalid input"
+    option "4" list
+    else do
+      let x = (read days :: Int)
+      putStr(displayCityNames (dryPlacesInXDays x list))
+      return list
+
+option "5" list = do
+  rainfallFigs <- getRainfallUpdates list []
+  let newList = updateAllPlaces rainfallFigs list
+  putStrLn (placesToString newList)
+  return newList
+
+option "6" list = do
+  putStr "Enter the name of the city you would like to remove: "
+  place <- getLine
+  if (placeExists place list) then do
+    let filteredList = removePlace place list
+    newPlace <- createPlace
+    let newList = addPlace newPlace filteredList
+    putStrLn (placesToString newList)
+    return newList
+    else do
+      putStrLn "City not found"
+      option "6" list
+
+option "7" list = do
+  let dryList = dryPlacesInXDays 1 list
+  coords <- getCoords
+  let closestPlace = getClosest coords dryList
+  putStrLn (placesToString [closestPlace])
+  return list
+
+option "8" list = do
+  clearScreen
+  displayMap list
+  writeAt (0,50) "Press enter to continue"
+  wait <- getLine
+  return list
+
+option _ list = do
+  putStrLn "Invalid input"
+  return list
+
+menuData :: [String]
+menuData = [
+  "\nEnter a number to complete that action",
+  "0 = Exit the program",
+  "1 = List the names of all places available",
+  "2 = Return the average rainfall of a given place",
+  "3 = Return all places and their 7-day rainfall figures",
+  "4 = Return a list of names for all places that were dry a given amount of days ago",
+  "5 = Update rainfall figures for all places",
+  "6 = Replace existing place with a new place",
+  "7 = Given a location find the closest place that was dry yesterday",
+  "8 = Display a rainfall map"
+  ]
+
+menu :: [Place] -> IO [Place]
+menu list = do
+  putStrLn (unlines menuData)
+  choice <- getLine
+  if choice == "0" then
+    return list
+    else do
+      clearScreen
+      newList <- option choice list
+      menu newList
+
+main :: IO ()
+main = do
+  raw <- readFile "places.txt"
+  let list = (read raw :: [Place])
+  clearScreen
+  putStr (displayCityNames list)
+  newList <- menu list
+  writeFile "places.txt" (show newList)
